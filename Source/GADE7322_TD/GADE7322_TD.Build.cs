c6004b85f5eae62a0e5d8b65f57e32e9dba325e7
DavidBarker05 <davidbarker9611@gmail.com>
2026-07-31 01:19:39 +0200

    setup

--- Source/GADE7322_TD/GADE7322_TD.Build.cs
+++ Source/GADE7322_TD/GADE7322_TD.Build.cs
@@ -7,14 +7,17 @@ public class GADE7322_TD : ModuleRules
 	public GADE7322_TD(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PublicDependencyModuleNames.AddRange(new string[]
+			{ "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] { "GADE7322_TD", "GADE7322_TD/Any", "GADE7322_TD/Events" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
