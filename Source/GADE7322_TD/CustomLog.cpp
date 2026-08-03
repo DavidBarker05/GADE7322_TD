53ea14d5278ceafca8b3a7aab37b1874ea7953c4
DavidBarker05 <davidbarker9611@gmail.com>
2026-08-03 18:38:59 +0200

    change custom utils to custom log

---
+++ Source/GADE7322_TD/CustomLog.cpp
@@ -0,0 +1,62 @@
+﻿#include "CustomLog.h"
+
+DEFINE_LOG_CATEGORY(LogCustom);
+
+void FCustomLog::Log(ELogVerbosity::Type Verbosity, const FString& Message)
+{
+#if WITH_EDITOR
+    FColor Colour;
+#endif
+    switch (Verbosity)
+    {
+        case ELogVerbosity::VeryVerbose:
+#if WITH_EDITOR
+            Colour = FColor::White;
+#endif
+            UE_LOG(LogCustom, VeryVerbose, TEXT("%s"), *Message);
+            break;
+        case ELogVerbosity::Verbose:
+#if WITH_EDITOR
+            Colour = FColor::Cyan;
+#endif
+            UE_LOG(LogCustom, Verbose, TEXT("%s"), *Message);
+            break;
+        case ELogVerbosity::Log:
+#if WITH_EDITOR
+            Colour = FColor::Green;
+#endif
+            UE_LOG(LogCustom, Log, TEXT("%s"), *Message);
+            break;
+        case ELogVerbosity::Display:
+#if WITH_EDITOR
+            Colour = FColor::Green;
+#endif
+            UE_LOG(LogCustom, Display, TEXT("%s"), *Message);
+            break;
+        case ELogVerbosity::Warning:
+#if WITH_EDITOR
+            Colour = FColor::Yellow;
+#endif
+            UE_LOG(LogCustom, Warning, TEXT("%s"), *Message);
+            break;
+        case ELogVerbosity::Error:
+#if WITH_EDITOR
+            Colour = FColor::Red;
+#endif
+            UE_LOG(LogCustom, Error, TEXT("%s"), *Message);
+            break;
+        case ELogVerbosity::Fatal:
+#if WITH_EDITOR
+            Colour = FColor::Red;
+#endif
+            UE_LOG(LogCustom, Fatal, TEXT("%s"), *Message);
+            break;
+        default:
+            return;
+    }
+#if WITH_EDITOR
+    ELogVerbosity::Type CurrentVerbosity = LogCustom.GetVerbosity();
+    if (CurrentVerbosity < Verbosity) return;
+    GEngine->AddOnScreenDebugMessage(INDEX_NONE, 10.0f, Colour, Message);
+#endif
+}
---
+++ Source/GADE7322_TD/CustomLog.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogCustom, Log, All);
+
+#define TD_LOG(Verbosity, Format, ...) FCustomLog::Log(ELogVerbosity:: Verbosity, FString::Printf(Format __VA_OPT__(,) __VA_ARGS__))
+
+// Log a message at the Display verbosity
+#define TD_LOG_INFO(Format, ...) FCustomLog::Log(ELogVerbosity::Display, FString::Printf(Format __VA_OPT__(,) __VA_ARGS__))
+
+// Log a message at the Warning verbosity
+#define TD_LOG_WARN(Format, ...) FCustomLog::Log(ELogVerbosity::Warning, FString::Printf(Format __VA_OPT__(,) __VA_ARGS__))
+
+// Log a message at the Error verbosity
+#define TD_LOG_ERROR(Format, ...) FCustomLog::Log(ELogVerbosity::Error, FString::Printf(Format __VA_OPT__(,) __VA_ARGS__))
+
+// Log a message at the Fatal verbosity
+#define TD_LOG_FATAL(Format, ...) FCustomLog::Log(ELogVerbosity::Fatal, FString::Printf(Format __VA_OPT__(,) __VA_ARGS__))
+
+struct FCustomLog
+{
+    // Log a message
+    static void Log(ELogVerbosity::Type Verbosity, const FString& Message);
+};
