06eeadc50770958ad56b758d6f985e6118640f87
DavidBarker05 <davidbarker9611@gmail.com>
2026-08-03 18:39:15 +0200

    add pawns folder

--- Source/GADE7322_TD/GADE7322_TD.Build.cs
+++ Source/GADE7322_TD/GADE7322_TD.Build.cs
@@ -9,11 +9,23 @@ public class GADE7322_TD : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[]
-			{ "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput"
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] { });
 
-		PublicIncludePaths.AddRange(new string[] { "GADE7322_TD", "GADE7322_TD/Any", "GADE7322_TD/Events" });
+		PublicIncludePaths.AddRange(new string[]
+		{
+			"GADE7322_TD",
+			"GADE7322_TD/Any",
+			"GADE7322_TD/Events",
+			"GADE7322_TD/Pawns"
+		});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
