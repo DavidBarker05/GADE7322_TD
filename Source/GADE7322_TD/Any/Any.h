549e5cff3d9da33f28efcecc9fb66b94de5f7567
David Barker <164867676+DavidBarker05@users.noreply.github.com>
2026-09-01 23:52:11 +0200

    Add comments explaining how FAny works in c++
    I tried my best in what limited space I have with comments. Metaprogramming is a complicated topic

--- Source/GADE7322_TD/Any/Any.h
+++ Source/GADE7322_TD/Any/Any.h
@@ -17,96 +17,136 @@
 // I would do something different if Unreal's GC wouldn't get upset like seen in my SDL engine here:
 // https://github.com/DavidBarker05/SDL-Game/blob/7b7faec4508d3ad18f7a164791e3fca19f9ba05c/Engine/src/Core/Events/InputEvent.h
 
+// I realised I should probably explain myself since this code is kinda complicated so I'll do my
+// best to explain it
+
+// We will be using metaprogramming and specialisation to allow nearly any type to be used with
+// FAny. Specialisation, basically just allows you to write a base template and then override that
+// for specific types. This allows me to work with more than just USTRUCTs which are technically
+// the only type FAny stores, we just convert types to their respective struct found in
+// CustomStructs.h
+
+// Metaprogramming, specialisation and SFINAE are all used in this. They're way to complicated to
+// be explained by comment but information can be found online. Basically, C++ templates are
+// Turing complete and evaluated at compile time and we use the compiler to enable and disable
+// certain parts of code based on what we provide in templates
+
+// Partial specialisation for TAnyType, used for USTRUCTS because they don't need a wrapper
+// Enable will be used later to only allow certain raw pointers and TObjectPtrs to be used
 template<typename T, typename Enable = void>
 struct TAnyType
 {
-    using Type = T;
+    using Type = T; // T is a USTRUCT so Type is just T
 };
 
+// Full specialisation for bool
+// Basically if bool is ever passed as a template argument to TAnyType it will use this
+// instead
 template<>
 struct TAnyType<bool>
 {
-    using Type = FBoolStruct;
+    using Type = FBoolStruct; // Use FBoolStruct instead of bool
 };
 
+// Full specialisation for uint8
 template<>
 struct TAnyType<uint8>
 {
-    using Type = FUint8Struct;
+    using Type = FUint8Struct; // Use FUint8Struct instead of uint8
 };
 
+// Full specialisation for int32
 template<>
 struct TAnyType<int32>
 {
-    using Type = FInt32Struct;
+    using Type = FInt32Struct; // Use FInt32Struct instead of int32
 };
 
+// Full specialisation for int64
 template<>
 struct TAnyType<int64>
 {
-    using Type = FInt64Struct;
+    using Type = FInt64Struct; // Use FInt64Struct instead of int64
 };
 
+// Full specialisation for float
 template<>
 struct TAnyType<float>
 {
-    using Type = FFloatStruct;
+    using Type = FFloatStruct; // Use FFloatStruct instead of float
 };
 
+// Full specialisation for FString
 template<>
 struct TAnyType<FString>
 {
-    using Type = FFStringStruct;
+    using Type = FFStringStruct; // Use FFStringStruct instead of FString
 };
 
+// Full specialisation for const char* (basically any string text written like
+// "Lorem Ipsum")
 template<>
 struct TAnyType<const char*>
 {
-    using Type = FFStringStruct;
+    using Type = FFStringStruct; // Use FFStringStruct instead of const char*
 };
 
+// Full specialisation for char* (basically just array of chars that is
+// modifiable)
 template<>
 struct TAnyType<char*>
 {
-    using Type = FFStringStruct;
+    using Type = FFStringStruct; // Use FFStringStruct instead of char*
 };
 
+// Full specialisation for const WIDECHAR* (basically any string of text
+// written like L"Lorem Ipsum" or TEXT("Lorem Ipsum"))
 template<>
 struct TAnyType<const WIDECHAR*>
 {
-    using Type = FFStringStruct;
+    using Type = FFStringStruct; // Use FFStringStruct instead of const WIDECHAR*
 };
 
+// Full specialisation for WIDECHAR* (basically just array of WIDECHARs that is
+// modifiable)
 template<>
 struct TAnyType<WIDECHAR*>
 {
-    using Type = FFStringStruct;
+    using Type = FFStringStruct; // Use FFStringStruct instead of WIDECHAR*
 };
 
+// Full specialisation for FName
 template<>
 struct TAnyType<FName>
 {
-    using Type = FFNameStruct;
+    using Type = FFNameStruct; // Use FFNameStruct instead of FName
 };
 
+// Full specialisation for FText
 template<>
 struct TAnyType<FText>
 {
-    using Type = FFTextStruct;
+    using Type = FFTextStruct; // Use FFTextStruct instead of FText
 };
 
+// Specialisation for T = TObjectPtr<...>
+// TEnableIf only enables the code if the TObjectPtr points to a
+// UObject or a type that derives from UObject. Hides it otherwise
 template<typename T>
 struct TAnyType<T, typename TEnableIf<TIsTObjectPtr_V<T> &&
                                       TIsDerivedFrom<typename TRemoveObjectPointer<T>::Type, UObject>::IsDerived>::Type>
 {
-    using Type = FUObjectStruct;
+    using Type = FUObjectStruct; // Use FUObjectStruct instead of TObjectPtr<...>
 };
 
+// Specialisation of ...*
+// TEnableIf only enables the code if T = UObject* or what it
+// points to derives from UObject. Hides it otherwise
 template<typename T>
 struct TAnyType<T, typename TEnableIf<TIsPointer<T>::Value &&
                                       TIsDerivedFrom<typename TRemovePointer<T>::Type, UObject>::IsDerived>::Type>
 {
-    using Type = FUObjectStruct;
+    using Type = FUObjectStruct; // Use FUObjectStruct instead of ...*
 };
 
 /*
@@ -137,6 +177,7 @@ struct GADE7322_TD_API FAny
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite)
     FInstancedStruct Value;
+    // ^ FInstancedStruct stores any USTRUCT
 
     FAny() = default;
     FAny(const FAny&) = default;
@@ -146,16 +187,22 @@ struct GADE7322_TD_API FAny
 
     FAny(FInstancedStruct&& InValue) : Value(MoveTemp(InValue)) { }
 
+    // Copy constructor. Stores InValue in Value. Uses T if it's as struct or the struct
+    // equivalent if not
     template<typename T>
     FAny(const T& InValue) : Value(FInstancedStruct::Make<typename TAnyType<T>::Type>(InValue))
     {
     }
 
+    // Move constructor. Moves InValue into Value. Uses T if it's as struct or the struct
+    // equivalent if not. Don't enable if T is FAny
     template<typename T, TEnableIf<!std::is_same_v<TDecay<T>, FAny>>>
     FAny(T&& InValue) : Value(FInstancedStruct::Make<typename TAnyType<TDecay<T>>::Type>(Forward<T>(InValue)))
     {
     }
 
+    // Forwarding constructor. Makes Value store Type if it is a struct, or the struct
+    // equivalent if not. Forwards the params into the constructor for the struct
     template<typename Type, typename... Args>
     FAny(Args&&... Params) : Value(FInstancedStruct::Make<typename TAnyType<Type>::Type>(Forward<Args>(Params)...))
     {
@@ -170,6 +217,7 @@ struct GADE7322_TD_API FAny
         return *this;
     }
 
+    // Copy assignment operator. See copy constructor for how it works
     template<typename T>
     FAny& operator=(const T& InValue)
     {
@@ -177,6 +225,7 @@ struct GADE7322_TD_API FAny
         return *this;
     }
 
+    // Move assignment operator. See move constructor for how it works
     template<typename T, TEnableIf<!std::is_same_v<TDecay<T>, FAny>>>
     FAny& operator=(T&& InValue)
     {
@@ -186,25 +235,28 @@ struct GADE7322_TD_API FAny
 
     bool IsValid() const { return Value.IsValid(); }
 
+    // Get a mutable pointer to the underlying data
     template<typename T>
     T* Get()
     {
-        using StoredType = TAnyType<T>::Type;
-        if constexpr (std::is_same_v<StoredType, FUObjectStruct>)
+        using StoredType = TAnyType<T>::Type; // The struct type
+        if constexpr (std::is_same_v<StoredType, FUObjectStruct>) // Data is UObject ptr
         {
             FUObjectStruct* Wrapper = Value.GetMutablePtr<FUObjectStruct>();
-            if (!Wrapper) return nullptr;
-            if constexpr (TIsTObjectPtr_V<T>)
+            if (!Wrapper) return nullptr; // Value does not hold a FUObjectStruct
+            if constexpr (TIsTObjectPtr_V<T>) // TObjectPtr<...>
             {
-                using PointedType = TRemoveObjectPointer<T>::Type;
-                if (!Cast<PointedType>(Wrapper->Value)) return nullptr;
+                using PointedType = TRemoveObjectPointer<T>::Type; // The ...
+                if (!Cast<PointedType>(Wrapper->Value))
+                    return nullptr; // If can't cast to PointedType return nullptr
             }
-            else
+            else // ...*
             {
-                using PointedType = TRemovePointer<T>::Type;
-                if (!Cast<PointedType>(Wrapper->Value.Get())) return nullptr;
+                using PointedType = TRemovePointer<T>::Type; // ...
+                if (!Cast<PointedType>(Wrapper->Value.Get()))
+                    return nullptr; // If can't cast to PointeType return nullptr
             }
-            return reinterpret_cast<T*>(&Wrapper->Value);
+            return reinterpret_cast<T*>(&Wrapper->Value); // Reinterpret the address to the underlying data as T* and return it
         }
         else if constexpr (std::is_same_v<T, StoredType>) return Value.GetMutablePtr<T>(); // T is a USTRUCT
         else
@@ -214,9 +266,11 @@ struct GADE7322_TD_API FAny
         }
     }
 
+    // Get an immutable pointer to the underlying data
     template<typename T>
     const T* Get() const
     {
+        // See the mutable Get for how this works. It's the same
         using StoredType = TAnyType<T>::Type;
         if constexpr (std::is_same_v<StoredType, FUObjectStruct>)
         {
@@ -242,6 +296,7 @@ struct GADE7322_TD_API FAny
         }
     }
 
+    // Copy Set. See copy constructor for how it works
     template<typename T>
     FAny& Set(const T& InValue)
     {
@@ -249,6 +304,7 @@ struct GADE7322_TD_API FAny
         return *this;
     }
 
+    // Move Set. See move constructor for how it works
     template<typename T>
     FAny& Set(T&& InValue)
     {
